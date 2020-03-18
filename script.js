435c234f5bbc232090aec377c9a1d7c057b7dd5c
drospond <david.rospond@gmail.com>
2020-03-18 10:40:38 -0400

    added API key variable

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var apiKeyEventbrite = "CJNTVLROFG7S4LXZ46G2";
+    var apiKeyHotels = "3c2c5f64b8msh8750e09015167e8p191250jsn71fd26617126";
+    var apiKeyFlights = "fe9a566d2984e0bfc2f4a6330188eaac";
+
+
+
+});
